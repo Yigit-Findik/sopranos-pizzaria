e9b1a440195097db3e55705d2d58950b11eb6d8b
Yigit <yigit8@live.nl>
2021-02-26 10:46:14 +0100

    Custom height/widths + custom media querry

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,45 @@ module.exports = {
   },
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      height: {
+        //custom heights added for smoother development
+        112: '28rem',
+        120: '30rem',
+        128: '32rem',
+        136: '34rem',
+        144: '36rem',
+        152: '38rem',
+        190: '47.5rem',
+        240: '60rem',
+        256: '62rem',
+        300: '73rem',
+        325: '79rem',
+        350: '85rem',
+        375: '91rem'
+      },
+
+      //custom width added for smoother development
+      width: {
+        112: '28rem',
+        120: '30rem',
+        128: '32rem',
+        136: '34rem',
+        144: '36rem',
+        152: '38rem',
+        190: '47.5rem',
+        240: '60rem',
+        256: '62rem',
+        300: '73rem',
+        325: '79rem',
+        350: '85rem',
+        375: '91rem'
+      },
+
+      screens: {
+        'tel': '400px',
+      },
+    },
   },
   variants: {
     extend: {},
